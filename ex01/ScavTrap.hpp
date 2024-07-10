6f6a09e3872ce1712b76f8a570c8204b9ef51f40
aklein <aklein@student.hive.fi>
2024-07-10 21:43:04 +0300

    ScavTrap class as per subject

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -5,10 +5,14 @@
 class ScavTrap : public ClapTrap
 {
 public:
-	ScavTrap();
+	ScavTrap(std::string name);
 	ScavTrap(const ScavTrap& other);
 	ScavTrap& operator=(const ScavTrap& other);
 	~ScavTrap();
+
+	void attack(const std::string& target);
+	void guardGate(void);
+
 
 private:
 
